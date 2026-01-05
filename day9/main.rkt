1d707a3192f8f05dbf99778742b55ea9cf0dc362
Yiyun Liu <yliu8e5f7@protonmail.com>
2026-01-05 01:02:39 -0500

    Add wip solution to day9 p2

--- day9/main.rkt
+++ day9/main.rkt
@@ -2,19 +2,17 @@
 (require racket/generator)
 
 (define (parse-from-port port)
-  (generator
-   ()
-   (for ([line (sequence-map
-                string-trim
-                (in-lines port))]
-         #:break (string=? line ""))
-     (define result (regexp-match #px"(\\d+),(\\d+)" line))
-     (yield (cons (string->number (second result))
-                  (string->number (third result)))))))
+  (for ([line (sequence-map
+               string-trim
+               (in-lines port))]
+        #:break (string=? line ""))
+    (define result (regexp-match #px"(\\d+),(\\d+)" line))
+    (yield (cons (string->number (second result))
+                 (string->number (third result))))))
 
 (define (main port)
   (define points
-    (for/vector ([point (in-producer (parse-from-port port) (void))])
+    (for/vector ([point (in-generator (parse-from-port port))])
       point))
   (define size (vector-length points))
   (for*/fold
@@ -24,3 +22,30 @@
     (match-define (cons x0 y0) (vector-ref points i))
     (match-define (cons x1 y1) (vector-ref points j))
     (max max-area (* (add1 (abs (- x0 x1))) (add1 (abs (- y0 y1)))))))
+
+
+;; - lines (same y coord)
+;; x1 >= x0
+(struct Horizontal (y x0 x1) #:transparent)
+;; | lines (same x coord)
+;; y0 >= y1
+(struct Vertical (x y0 y1) #:transparent)
+
+
+(define (points->line xy0 xy1)
+  (match-define (cons x0 y0) xy0)
+  (match-define (cons x1 y1) xy1)
+  (cond
+    ([eqv? x0 x1]
+     (Vertical x0 (min y0 y1) (max y0 y1)))
+    (else
+     (Horizontal y0 (min x0 x1) (max x0 x1)))))
+
+(define (tiles->lines tiles)
+  (for/fold
+      ([last-tile
+        (vector-ref tiles (sub1 (vector-length tiles)))]
+       [lines
+        '()])
+      ([tile tiles])
+    (values tile (cons (points->line tile last-tile) lines))))
